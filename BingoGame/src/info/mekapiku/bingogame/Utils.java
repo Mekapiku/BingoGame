fbf646a946d5029a43820bd3efbd363a58c11242
Mekapiku <mekapiku@gmail.com>
2018-05-27 14:18:07 +0900

    Delete old files

--- BingoGame/src/info/mekapiku/bingogame/Utils.java
+++
@@ -1,9 +0,0 @@
-package info.mekapiku.bingogame;
-public final class Utils {
-	public static final boolean isMac() {
-        if (System.getProperty("os.name").indexOf("Mac") != -1)
-            return true;
-        else
-            return false;
-	}
-}
